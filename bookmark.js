9332a34e7ad7f2fd2bef0f49a3b69289b08afde9
Seth Coussens <seth.coussens@nthplus.io>
2019-05-10 12:18:40 -0500

    initial test script

---
+++ bookmark.js
@@ -0,0 +1,8 @@
+javascript: (function() {
+  var jsCode = document.createElement('script');
+  jsCode.setAttribute(
+    'src',
+    'https://github.com/scoussens/bookmarklet-cc-cleanup/blob/master/bookmarklet-cc-cleanup.js'
+  );
+  document.body.appendChild(jsCode);
+})();
